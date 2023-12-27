ea6924785050b7ea4f95eef18c907b325af8d055
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2023-12-27 09:44:54 +0530

    build: basic layout

---
+++ components/base/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navbar = () => {
+  return <nav className='bg-vscode-background-secondary w-full h-9'></nav>;
+};
+
+export default Navbar;
---
+++ components/base/Sidebar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return <div className='bg-vscode-background-secondary w-2/6'>Sidebar</div>;
+};
+
+export default Sidebar;
